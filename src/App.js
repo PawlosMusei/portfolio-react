651de534f50195690ade03fcdeccca6cbb430c44
Pawlos <9020097@student.zadkine.nl>
2023-12-13 10:35:47 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./Pages/contact";
 import About from "./Pages/About";
-import Project from "./Pages/Project"
+import Project from "./Pages/Project";
 import Home from "./Pages/Home";
 import Nav from "./componeten/Nav";
 
@@ -11,13 +11,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/portfolio-react" element={<Nav />}>
-            <Route index element={<Home />} />
-            <Route path="/portfolio-react/home" element={<Home />} />
-            <Route path="/portfolio-react/about" element={<About />} />
-            <Route path="/portfolio-react/project" element={<Project />} />
-            <Route path="/portfolio-react/contact" element={<Contact />} />
-          </Route>
+          <Route index element={<Home />} />
+          <Route path="/portfolio-react/home" element={<Home />} />
+          <Route path="/portfolio-react/" element={<Home />} />
+          <Route path="/portfolio-react/about" element={<About />} />
+          <Route path="/portfolio-react/project" element={<Project />} />
+          <Route path="/portfolio-react/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
